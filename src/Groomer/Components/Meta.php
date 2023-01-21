9634e89d55bda0ed8d6992294813940baee3073a
Isaac Machakata <108731754+im-machakata@users.noreply.github.com>
2023-01-21 06:01:49 +0200

    feature: add meta

--- src/Groomer/Components/Meta.php
+++ src/Groomer/Components/Meta.php
@@ -3,50 +3,25 @@
 namespace Caasi\Groomer\Components;
 
 /**
- * Basic structure of a css file
+ * Basic structure of a meta file
  * @author Isaac Machakata <isaac@caasi.co.zw>
  * @link https://github.com/caasi-co-zw/groomer
  * @version 1.0.0
  */
 class Meta {
     private $name;
-    private $href;
-    private $async;
-    private $media;
-    private $version;
+    private $property;
+    private $content;
 
-    public function __construct() {
-        $this->media = 'all';
-        $this->async = true;
-    }
-    public function setName(string $name) {
+    public function __construct($name,$content) {
         $this->name = $name;
+        $this->content = $content;
+        $this->property = 'name';
     }
-    public function setSource(string $src) {
-        $this->href = $src;
+    public function setProperty(bool $enable){
+        $this->property = $enable ? 'property' : 'name';
     }
-    public function setAsynchronous(bool $enable) {
-        $this->async = $enable;
-    }
-    public function setMedia(string $media) {
-        $this->media = $media;
-    }
-    public function setVersion(string $version) {
-        $this->version = $version;
-    }
-    public function getName(): string {
-        return $this->name;
-    }
-    public function getSource(): string {
-        return $this->href;
-    }
-    public function isAsynchronous(): bool {
-        return $this->async;
-    }
-    public function getMedia(): string {
-        return $this->media;
-    }
-    public function getVersion(): string {
-        return $this->version;
+    public function __toString(){
+        return sprintf('<meta %s="%s" content="%s">',$this->property,$this->content);
     }
 }
