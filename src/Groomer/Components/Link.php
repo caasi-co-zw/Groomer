e4263033ecb563fbb906e21d80d42091dc65da43
Isaac Machakata <isaac@caasi.co.zw>
2023-02-19 23:04:28 +0200

    feature: link tag

---
+++ src/Groomer/Components/Link.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Caasi\Groomer\Components;
+
+/**
+ * Basic structure of a link
+ * @author Isaac Machakata <isaac@caasi.co.zw>
+ * @link https://github.com/caasi-co-zw/groomer
+ * @version 1.0.0
+ */
+class Link
+{
+    const NAME = 'name';
+    const HREF = 'href';
+    const CONTENT = 'content';
+    const PROPERTY = 'property';
+    const HTTP_EQUIV = 'http-equiv';
+    private $results = '';
+    private $elementName = 'link';
+
+    /**
+     * Pass in a list of keys and their values.
+     */
+    public function __construct(...$values)
+    {
+        $keys = $strings = [];
+        foreach ($values as $value) :
+            if (is_string($value)) :
+                $strings[] = sprintf(' %s', $value);
+            elseif (is_array($value)) :
+                $keys[] = sprintf(' %s="%s"', $value[0], $value[1]);
+            endif;
+        endforeach;
+        sort($strings);
+        $this->results = implode(' ', $keys) . implode(' ', $strings);
+        print($this->__toString());
+    }
+    public function __toString()
+    {
+        return sprintf('<%s %s>', $this->elementName, trim($this->results));
+    }
+}
