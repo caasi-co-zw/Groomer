edafd7ba4a4f739cf73342a1d1f25b3f5cdb055f
Isaac Machakata <isaac@caasi.co.zw>
2023-03-21 21:17:32 +0200

    feature: style component

---
+++ src/Groomer/Components/Style.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Groomer\Components;
+
+/**
+ * Basic structure of a script
+ * @author Isaac Machakata <isaac@caasi.co.zw>
+ * @link https://github.com/caasi-co-zw/groomer
+ * @version 1.0.0
+ */
+class Style extends BaseComponent
+{
+    const TYPE = 'type';
+    const TYPE_CSS = 'type="text/css"';
+    private $elementName = 'style';
+}
