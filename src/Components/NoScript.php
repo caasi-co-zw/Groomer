570ed0c8aa54da5d561825ab3e668939f1837520
Isaac Machakata <isaac@caasi.co.zw>
2023-03-23 00:33:11 +0200

    feature: no script

---
+++ src/Components/NoScript.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Groomer\Components;
+
+/**
+ * Basic structure of a link
+ * @author Isaac Machakata <isaac@caasi.co.zw>
+ * @link https://github.com/caasi-co-zw/groomer
+ * @version 1.0.0
+ */
+class NoScript extends BaseComponent
+{
+    protected $elementName = 'noscript';
+    protected $closeTag = true;
+}
