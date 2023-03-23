c49d4e91d8a63c53009eaded65519767efb64a53
Isaac Machakata <isaac@caasi.co.zw>
2023-03-23 15:53:04 +0200

    fix: make element name protected

--- src/Components/Style.php
+++ src/Components/Style.php
@@ -12,5 +12,5 @@ class Style extends BaseComponent
 {
     const TYPE = 'type';
     const TYPE_CSS = 'type="text/css"';
-    private $elementName = 'style';
+    protected $elementName = 'style';
 }
