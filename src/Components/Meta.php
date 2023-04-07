61c206255eab5ba7c1692f35457fcf1887a4a3c8
Isaac Machakata <108731754+im-machakata@users.noreply.github.com>
2023-04-07 04:56:54 +0200

    refactor: add color schemes

--- src/Components/Meta.php
+++ src/Components/Meta.php
@@ -21,5 +21,10 @@ class Meta extends BaseComponent
     const FACEBOOK_SITENAME = 'og:site_name';
     const FACEBOOK_IMAGE_ALT = 'og:image_alt';
     const FACEBOOK_DESCRIPTION = 'og:description';
+    const PREFER_COLOR = 'name="prefer-color-scheme"';
+    const PREFER_COLOR_DARK = 'dark';
+    const PREFER_COLOR_LIGHT = 'light';
+    const PREFER_COLOR_LIGHT_THEME = 'name="prefer-color-scheme" content="light"';
+    const PREFER_COLOR_DARK_THEME = 'name="prefer-color-scheme" content="dark"';
     protected $closeTag = false;
 }
